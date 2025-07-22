14ced2470b8fdd81e2bfbdecc3918cabdf83a237
yoandrisespinosa <89758543+yoandrisespinosa@users.noreply.github.com>
2025-07-22 20:17:28 -0300

    update

--- Dev_Device/assets/MOTOR/Scripts/newscript/newscript.py
+++ Dev_Device/assets/MOTOR/Scripts/newscript/newscript.py
@@ -17,4 +17,15 @@ my_random_integer = random.randint(1, 100)  # generates a random integer between
 number1 = my_random_integer
 number2 = my_random_integer
 result = sum_two_numbers(number1, number2)
-debug(f"The sum of {number1} and {number2} is {result}")
+debug(f"The sum of {number1} and {number2} is {result}") 
+# script on Cloud
+
+# Welcome to SORBA SDE Python Editor, programming in python language integrated with SORBA.
+# Use sde["ASSET.GROUP.TAG"] to read or write SDE tags or sde[ASSET+".GROUP.TAG"] for generic code
+# based on Asset property.
+# Declare as global for sharing imported modules, functions or variables between scripts.
+# Use debug(args) or debug_sde (sde_tags_names) to log in the debug.log file with timestamp.
+# Look for built-in SDE scripts functions and other SDE python modules like timer, counter, OEE for easy
+# For importing custom modules from any path add in OnStart script: import sys; sys.path.append('new path')
+# scripting.
+# Enter your python code here.
