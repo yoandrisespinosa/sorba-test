5ef5394b63fd1cf38139fce1e5250492e3070e39
yoandrisespinosa <89758543+yoandrisespinosa@users.noreply.github.com>
2025-07-24 18:02:53 -0300

    update comment

--- SDE_TESTING_135/assets/runScript/Scripts/scrapping/scapping.py
+++ SDE_TESTING_135/assets/runScript/Scripts/scrapping/scapping.py
@@ -1,6 +1,6 @@
 # @Async
 
-
+# The Async scripts are used when executing operations like HTTP request that takes long time
 
 # Call main directly (Sorba may call it depending on execution control setting)
 Callweb()
