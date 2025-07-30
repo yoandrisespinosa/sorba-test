aeb5850d877d0f6b646c7f106a7ae6bb89a3e97e
yoandrisespinosa <89758543+yoandrisespinosa@users.noreply.github.com>
2025-07-30 14:06:34 -0300

    jajajaj

---
+++ SDE_TESTING_135/assets/MOTOR/Scripts/Aguacato/Aguacato.py
@@ -0,0 +1,8 @@
+# Welcome to Python Editor integrated with SORBA. Use sde["ASSET.GROUP.TAG"] to read or write tags. 
+# Set the execution control: On Start, Cyclic, On Stop, Module, On Schedule, On Trigger and Callable.
+# Define global variables using glb.add(name=value,...),access as glb.name.Also use global keyword to share variables, functions,lib.
+# For debugging, use debug(*args) and visualize the results in the Debug tab. To monitor runtime, check Statistics tab.
+# Look for built-in script functions and modules like timer, counter, track, cyclic_list, and time_counter (RepeatTimer,Heartbeat, Async).
+# To import Python modules outside of Script-Engine, add to the “On Start” script: import sys; sys.path.append ('new path')
+# Enter your Python code here.For more information related with quality,channel,glb, packages installation, please refer to the help file.
+ahahahahha
